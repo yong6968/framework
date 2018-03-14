f2c1cbe67eec52ce652ef2c2765bc9b4bdac076d
linzhiyong <linzhiyong@heshidai.com>
2018-03-14 11:51:07 +0800

    java实现2叉树

---
+++ src/test/java/framework/a.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 ShenZhen HeShiDai Co.,Ltd All Rights Reserved.
+ * 未经本公司正式书面同意，其他任何个人、团体不得使用、复制、修改或发布本软件.
+ * 版权所有深圳合时代金融服务有限公司 www.heshidai.com.
+ */
+package framework;
+
+/**
+ * TODO:类功能介绍
+ * @version 2018年3月12日上午10:05:04
+ * @author zhiyong.lin
+ */
+public class a {
+    
+    /**
+     * TODO:方法描述
+     * @version 2018年3月12日上午10:05:04
+     * @author zhiyong.lin
+     * @param args
+     */
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int i =1;
+        i ^= 1;
+        System.out.println(i);
+    }
+    
+}
